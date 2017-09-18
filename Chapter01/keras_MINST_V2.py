a5e2b4613d571baa37c702663471ec8a22eab968
SnowGushiGit <snow.akogi.pgel@gmail.com>
2017-09-18 15:05:40 +0900

    Update mmnist train
    For tensorboard visualize

--- Chapter01/keras_MINST_V2.py
+++ Chapter01/keras_MINST_V2.py
@@ -6,22 +6,24 @@ from keras.layers.core import Dense, Activation
 from keras.optimizers import SGD
 from keras.utils import np_utils
 from make_tensorboard import make_tensorboard
+import tensorflow as tf
 
 np.random.seed(1671)  # for reproducibility
 
 # network and training
 NB_EPOCH = 20
+NB_EPOCH = 10
 BATCH_SIZE = 128
 VERBOSE = 1
 NB_CLASSES = 10   # number of outputs = number of digits
-OPTIMIZER = SGD() # optimizer, explained later in this chapter
+OPTIMIZER = SGD()  # optimizer, explained later in this chapter
 N_HIDDEN = 128
-VALIDATION_SPLIT=0.2 # how much TRAIN is reserved for VALIDATION
+VALIDATION_SPLIT = 0.2  # how much TRAIN is reserved for VALIDATION
 
 # data: shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-#X_train is 60000 rows of 28x28 values --> reshaped in 60000 x 784
+# X_train is 60000 rows of 28x28 values --> reshaped in 60000 x 784
 RESHAPED = 784
 #
 X_train = X_train.reshape(60000, RESHAPED)
@@ -29,7 +31,7 @@ X_test = X_test.reshape(10000, RESHAPED)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
-# normalize 
+# normalize
 X_train /= 255
 X_test /= 255
 print(X_train.shape[0], 'train samples')
@@ -43,25 +45,39 @@ Y_test = np_utils.to_categorical(y_test, NB_CLASSES)
 # 10 outputs
 # final stage is softmax
 
-model = Sequential()
-model.add(Dense(N_HIDDEN, input_shape=(RESHAPED,)))
-model.add(Activation('relu'))
-model.add(Dense(N_HIDDEN))
-model.add(Activation('relu'))
-model.add(Dense(NB_CLASSES))
-model.add(Activation('softmax'))
-model.summary()
+with tf.name_scope('Model') as scope:
+    model = Sequential()
+    with tf.name_scope('Dense') as scope:
+        model.add(Dense(N_HIDDEN, input_shape=(RESHAPED,)))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=OPTIMIZER,
-              metrics=['accuracy'])
+    with tf.name_scope('Relu') as scope:
+        model.add(Activation('relu'))
 
-callbacks = [make_tensorboard(set_dir_name='keras_MINST_V3')]
+    with tf.name_scope('Dense2') as scope:
+        model.add(Dense(N_HIDDEN))
 
-history = model.fit(X_train, Y_train,
-                    batch_size=BATCH_SIZE, epochs=NB_EPOCH,
-                    callbacks=callbacks,
-                    verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+    with tf.name_scope('Relu2') as scope:
+        model.add(Activation('relu'))
+
+    with tf.name_scope('Dense3') as scope:
+        model.add(Dense(NB_CLASSES))
+
+    with tf.name_scope('SoftMax') as scope:
+        model.add(Activation('softmax'))
+    model.summary()
+
+with tf.name_scope('ModelCompile') as scope:
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=OPTIMIZER,
+                  metrics=['accuracy'])
+
+callbacks = [make_tensorboard(set_dir_name='keras_MINST_V2')]
+
+with tf.name_scope('TrainingModel') as scope:
+    model.fit(X_train, Y_train,
+              batch_size=BATCH_SIZE, epochs=NB_EPOCH,
+              callbacks=callbacks,
+              verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
 
 score = model.evaluate(X_test, Y_test, verbose=VERBOSE)
 print("\nTest score:", score[0])
--- Chapter01/keras_MINST_V3.py
+++ Chapter01/keras_MINST_V3.py
@@ -6,6 +6,7 @@ from keras.layers.core import Dense, Dropout, Activation
 from keras.optimizers import SGD
 from keras.utils import np_utils
 from make_tensorboard import make_tensorboard
+import tensorflow as tf
 
 
 np.random.seed(1671)  # for reproducibility
@@ -15,15 +16,15 @@ NB_EPOCH = 250
 BATCH_SIZE = 128
 VERBOSE = 1
 NB_CLASSES = 10   # number of outputs = number of digits
-OPTIMIZER = SGD() # optimizer, explained later in this chapter
+OPTIMIZER = SGD()  # optimizer, explained later in this chapter
 N_HIDDEN = 128
-VALIDATION_SPLIT=0.2 # how much TRAIN is reserved for VALIDATION
+VALIDATION_SPLIT = 0.2  # how much TRAIN is reserved for VALIDATION
 DROPOUT = 0.3
 
 # data: shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-#X_train is 60000 rows of 28x28 values --> reshaped in 60000 x 784
+# X_train is 60000 rows of 28x28 values --> reshaped in 60000 x 784
 RESHAPED = 784
 #
 X_train = X_train.reshape(60000, RESHAPED)
@@ -31,7 +32,7 @@ X_test = X_test.reshape(10000, RESHAPED)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
-# normalize 
+# normalize
 X_train /= 255
 X_test /= 255
 print(X_train.shape[0], 'train samples')
@@ -45,29 +46,47 @@ Y_test = np_utils.to_categorical(y_test, NB_CLASSES)
 # 10 outputs
 # final stage is softmax
 
-model = Sequential()
-model.add(Dense(N_HIDDEN, input_shape=(RESHAPED,)))
-model.add(Activation('relu'))
-model.add(Dropout(DROPOUT))
-model.add(Dense(N_HIDDEN))
-model.add(Activation('relu'))
-model.add(Dropout(DROPOUT))
-model.add(Dense(NB_CLASSES))
-model.add(Activation('softmax'))
-model.summary()
+with tf.name_scope('Model') as scope:
+    model = Sequential()
+    with tf.name_scope('Dense') as scope:
+        model.add(Dense(N_HIDDEN, input_shape=(RESHAPED,)))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=OPTIMIZER,
-              metrics=['accuracy'])
+    with tf.name_scope('Relu') as scope:
+        model.add(Activation('relu'))
+
+    with tf.name_scope('Dropout') as scope:
+        model.add(Dropout(DROPOUT))
+
+    with tf.name_scope('Dense2') as scope:
+        model.add(Dense(N_HIDDEN))
+
+    with tf.name_scope('Relu2') as scope:
+        model.add(Activation('relu'))
+
+    with tf.name_scope('Dropout2') as scope:
+        model.add(Dropout(DROPOUT))
+
+    with tf.name_scope('Dense3') as scope:
+        model.add(Dense(NB_CLASSES))
+
+    with tf.name_scope('Softmax') as scope:
+        model.add(Activation('softmax'))
+
+    model.summary()
+
+with tf.name_scope('ModelCompile') as scope:
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=OPTIMIZER,
+                  metrics=['accuracy'])
 
 callbacks = [make_tensorboard(set_dir_name='keras_MINST_V3')]
 
-history = model.fit(X_train, Y_train,
-                    batch_size=BATCH_SIZE, epochs=NB_EPOCH,
-                    callbacks=callbacks,
-                    verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+with tf.name_scope('TrainingModel') as scope:
+    model.fit(X_train, Y_train,
+              batch_size=BATCH_SIZE, epochs=NB_EPOCH,
+              callbacks=callbacks,
+              verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
 
 score = model.evaluate(X_test, Y_test, verbose=VERBOSE)
 print("\nTest score:", score[0])
 print('Test accuracy:', score[1])
-
