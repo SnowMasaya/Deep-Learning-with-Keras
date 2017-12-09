de46a83882c19860d3d126062805b70036d7749e
SnowGushiGit <snow.akogi.pgel@gmail.com>
2017-12-09 13:03:38 +0900

    # Update chpter01
    For update tensorboard embeddings

--- Chapter01/keras_MINST_V4_model_tensorboard.py
+++ Chapter01/keras_MINST_V4_model_tensorboard.py
@@ -7,9 +7,11 @@ from keras.optimizers import RMSprop
 from keras.optimizers import Adam
 from keras.utils import np_utils
 from make_tensorboard_model import make_tensorboard
+from make_tensorboard_model import TensorResponseBoard
 import tensorflow as tf
 import argparse
-
+from PIL import Image
+import os
 
 parser = argparse.ArgumentParser(description="keras Mnist v4 model")
 
@@ -23,12 +25,12 @@ args = parser.parse_args()
 np.random.seed(1671)  # for reproducibility
 
 # network and training
-NB_EPOCH = 20
+NB_EPOCH = 10
 BATCH_SIZE = 128
 VERBOSE = 1
 NB_CLASSES = 10   # number of outputs = number of digits
 if args.optimizer == 'rmsprop':
-    OPTIMIZER = RMSprop()  # optimizer, explainedin this chapter
+    OPTIMIZER = RMSprop(lr=1,)  # optimizer, explainedin this chapter
 elif args.optimizer == 'adam':
     OPTIMIZER = Adam()  # optimizer, explainedin this chapter
 N_HIDDEN = 128
@@ -81,25 +83,47 @@ with tf.name_scope('Model') as scope:
         model.add(Dropout(DROPOUT))
 
     with tf.name_scope('Dense3') as scope:
-        model.add(Dense(NB_CLASSES))
+        model.add(Dense(NB_CLASSES, activation='softmax'))
 
-    with tf.name_scope('Softmax') as scope:
-        model.add(Activation('softmax'))
+    # with tf.name_scope('Softmax') as scope:
+    #     model.add(Activation('softmax'))
 
-    model.summary()
+model.summary()
 
-callbacks = [make_tensorboard(set_dir_name='keras_MINST_V4_' + args.optimizer)]
+callbacks, log_dir = make_tensorboard(set_dir_name='keras_MINST_V4_' + args.optimizer,
+                                      layer_name='dense_1',
+                                      metadata_file='metadata.tsv'
+                                      )
+
+tb = TensorResponseBoard(log_dir=log_dir, histogram_freq=1, batch_size=10,
+                         write_graph=True, write_grads=True, write_images=True,
+                         embeddings_freq=1,
+                         embeddings_layer_names=['dense_1'],
+                         embeddings_metadata='metadata.tsv',
+                         val_size=len(X_test), img_path='images.jpg', img_size=[28, 28])
+
+img_array = X_test.reshape(100, 100, 28, 28)
+img_array_flat = np.concatenate([np.concatenate([x for x in row], axis=1) for row in img_array])
+img = Image.fromarray(np.uint8(255 * (1. - img_array_flat)))
+img.save(os.path.join(log_dir, 'images.jpg'))
+np.savetxt(os.path.join(log_dir, 'metadata.tsv'), np.where(Y_test)[1], fmt='%d')
 
 with tf.name_scope('ModelCompile') as scope:
     model.compile(loss='categorical_crossentropy',
                   optimizer=OPTIMIZER,
                   metrics=['accuracy'])
 
+number_list = [i for i in range(1, 10)]
+
 with tf.name_scope('TrainingModel') as scope:
     model.fit(X_train, Y_train,
               batch_size=BATCH_SIZE, epochs=NB_EPOCH,
-              callbacks=callbacks,
+              callbacks=[tb],
               verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+
+with open('metadata.tsv', 'w', encoding='utf8') as f:
+    for name in number_list:
+        f.write('%s\n' % str(name))
 
 score = model.evaluate(X_test, Y_test, verbose=VERBOSE)
 print("\nTest score:", score[0])
